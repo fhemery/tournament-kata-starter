0b9b49ea5111716e073e83fda2f9a0eae1b9f508
Yann LE COZ <yann.lecoz@ynov.com>
2021-10-21 17:33:57 +0200

    Check issue 1

---
+++ apps/tournament/src/testing/tournament.spec.ts
@@ -0,0 +1,30 @@
+import { app } from '../app';
+import * as request from 'supertest';
+import { Tournament, TournamentPhaseType } from '../app/api/api-model';
+
+const exampleTournament = {
+  name: 'Unreal',
+} as Tournament;
+
+describe('/tournament endpoint', () => {
+  describe('[POST] when creating a tournament', () => {
+    it('should return the correct id', async () => {
+      const { body } = await request(app).post('/api/tournaments').send(exampleTournament).expect(201);
+
+      expect(body.id).not.toBeUndefined();
+    });
+
+    it("should have a name", async () => {
+      await request(app).post('/api/tournaments').send({}).expect(400);
+      await request(app).post('/api/tournaments').send({ name: "" }).expect(400);
+    });
+
+    it('should have stored the tournament', async () => {
+      const { body } = await request(app).post('/api/tournaments').send(exampleTournament).expect(201);
+
+      const get = await request(app).get(`/api/tournaments/${body.id}`).expect(200);
+
+      expect(get.body.name).toEqual(exampleTournament.name);
+    });
+  });
+});
