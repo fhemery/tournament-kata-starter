c8cfe9ce323f43d8e677bf50446c8b2634b577d6
Mockinbrd <marvyn.ap@gmail.com>
2021-10-21 16:48:47 +0200

    :rotating_light: : issue #1, #2, #3

---
+++ php/src/Controller/ParticipantController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/api/tournaments/{tournamentId}/participants", name="create_participant", methods={"POST"})
+     */
+    public function createTournamen(): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+}
--- php/tests/Acceptance/TournamentTest.php
+++ php/tests/Acceptance/TournamentTest.php
@@ -51,4 +51,6 @@ class TournamentTest extends ApiTestCase
 
         $this->assertResponseStatusCodeSame(404);
     }
+
+
 }
